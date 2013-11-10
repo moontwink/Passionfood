124be82bd69066fdae1e73fb239ecbebf10040a1
moontwink <chykuri@gmail.com>
2013-11-10 13:12:32 +0800

    third commit

---
+++ gen/mobicom/cc/passionfood/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package mobicom.cc.passionfood;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/mobicom/cc/passionfood/MainActivity.java
@@ -0,0 +1,39 @@
+package mobicom.cc.passionfood;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.search_action:
+//	            openSearch();
+	            return true;
+	        case R.id.action_settings:
+//	            openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+}
